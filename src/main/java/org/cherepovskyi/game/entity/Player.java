a360dc4a715f6d70c12a1eb52c53b25421ba5880
Dmytro <sxzaqwedc186@gmail.com>
2024-03-13 18:10:28 +0200

    second commit
    Repeated lessons 7
    Initial gradle config

--- src/main/java/org/cherepovskyi/game/entity/Player.java
+++ src/main/java/org/cherepovskyi/game/entity/Player.java
@@ -4,50 +4,128 @@ package org.cherepovskyi.game.entity;
 import org.cherepovskyi.game.main.GamePanel;
 import org.cherepovskyi.game.main.KeyHandler;
 
+import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 public class Player extends Entity {
-    GamePanel qp;
+    GamePanel gp;
     KeyHandler keyH;
 
-    public Player(GamePanel qp, KeyHandler keyH) {
-        this.qp = qp;
+    public final int screenX;
+    public final int screenY;
+
+    public Player(GamePanel gp, KeyHandler keyH) {
+        this.gp = gp;
         this.keyH = keyH;
 
+        screenX = gp.screenWidth/2;
+        screenY = gp.screenHeight/2;
+
         setDefaultValue();
+        getPlayerImage();
     }
 
     public void setDefaultValue() {
-        x = 100;
-        y = 100;
+        worldX = gp.tileSize*25 - gp.tileSize/2;
+        worldY = gp.tileSize*25 - gp.tileSize/2;
         speed = 4;
+        direction = "down";
+    }
+
+    public void getPlayerImage(){
+        try{
+            up1 = ImageIO.read(getClass().getResourceAsStream("up.png"));
+            up2 = ImageIO.read(getClass().getResourceAsStream("up2.png"));
+
+            left1 = ImageIO.read(getClass().getResourceAsStream("left.png"));
+            left2 = ImageIO.read(getClass().getResourceAsStream("left2.png"));
+
+            right1 = ImageIO.read(getClass().getResourceAsStream("right.png"));
+            right2 = ImageIO.read(getClass().getResourceAsStream("right2.png"));
+
+            down1 = ImageIO.read(getClass().getResourceAsStream("down.png"));
+            down2 = ImageIO.read(getClass().getResourceAsStream("down2.png"));
+
+        }catch(IOException e){
+            e.printStackTrace();
+        }
     }
 
     public void update() {
-        if (keyH.upPressed) {
-            y -= speed;
-        }
-        if (keyH.downPressed) {
-            y += speed;
-        }
-        if (keyH.leftPressed) {
-            x -= speed;
-        }
-        if (keyH.rightPressed) {
-            x += speed;
-        }
-        // diagonal speed normalization
-        if (keyH.upPressed && keyH.downPressed || keyH.upPressed && keyH.leftPressed || keyH.upPressed == true && keyH.rightPressed || keyH.downPressed && keyH.leftPressed || keyH.downPressed && keyH.rightPressed || keyH.leftPressed && keyH.rightPressed) {
-            speed = 3;
-        } else {
-            speed = 4;
-        }
+        if(keyH.upPressed || keyH.leftPressed || keyH.downPressed || keyH.rightPressed){
+            if (keyH.upPressed) {
+                direction = "up";
+                worldY -= speed;
+            }
+            if (keyH.downPressed) {
+                direction = "down";
+                worldY += speed;
+            }
+            if (keyH.leftPressed) {
+                direction = "left";
+                worldX -= speed;
+            }
+            if (keyH.rightPressed) {
+                direction = "right";
+                worldX += speed;
+            }
+            // diagonal speed normalization
+            if (keyH.upPressed && keyH.leftPressed || keyH.upPressed && keyH.rightPressed || keyH.downPressed && keyH.leftPressed || keyH.downPressed && keyH.rightPressed) {
+                speed = 3;
+            } else {
+                speed = 4;
+            }
 
+            spriteCounter++;
+            if(spriteCounter>10){
+                if(spriteNum == 1){
+                    spriteNum = 2;
+                }else {
+                    spriteNum = 1;
+                }
+                spriteCounter = 0;
+            }
+        }
     }
 
-    public void paintComponent(Graphics2D g2) {
+    public void draw(Graphics2D g2) {
 
-        g2.setColor(Color.white);
-        g2.fillRect(x, y, qp.tileSize, qp.tileSize);
+        //g2.setColor(Color.white);
+        //g2.fillRect(x, y, qp.tileSize, qp.tileSize);
+
+        BufferedImage image = null;
+        switch (direction){
+            case "up":
+                if(spriteNum == 1){
+                    image = up1;
+                }else {
+                    image = up2;
+                }
+                break;
+            case "down":
+                if(spriteNum == 1){
+                    image = down1;
+                }else {
+                    image = down2;
+                }
+                break;
+            case "left":
+                if(spriteNum == 1){
+                    image = left1;
+                }else {
+                    image = left2;
+                }
+                break;
+            case "right":
+                if(spriteNum == 1){
+                    image = right1;
+                }else {
+                    image = right2;
+                }
+                break;
+        }
+        g2.drawImage(image, screenX, screenY,gp.tileSize,gp.tileSize, null);
     }
 }
--- src/main/java/org/cherepovskyi/game/main/GamePanel.java
+++ src/main/java/org/cherepovskyi/game/main/GamePanel.java
@@ -1,6 +1,7 @@
 package org.cherepovskyi.game.main;
 
 import org.cherepovskyi.game.entity.Player;
+import org.cherepovskyi.game.tile.TileManager;
 
 import javax.swing.*;
 import java.awt.*;
@@ -13,17 +14,26 @@ public class GamePanel extends JPanel implements Runnable {
 
     public final int tileSize = originalTileSize * scale;
 
-    final int maxScreenCol = 16;
-    final int maxScreenRow = 12;
-    final int screenWidth = tileSize * maxScreenCol;
-    final int screenHeight = tileSize * maxScreenRow;
+    public final int maxScreenCol = 16;
+    public final int maxScreenRow = 12;
+    public final int screenWidth = tileSize * maxScreenCol;
+    public final int screenHeight = tileSize * maxScreenRow;
+
+    //WORLD SATTINGS
+    public final int maxWorldCol = 50;
+    public final int maxWorldRow = 50;
+    public final int WorldWidth = tileSize*maxWorldCol;
+    public final int WorldHeigt = tileSize*maxWorldRow;
+
+
 
     //FPS
     int FPS = 60;
 
+    TileManager tileM = new TileManager(this);
     KeyHandler keyH = new KeyHandler();
     Thread gameThread;
-    Player player = new Player(this, keyH);
+    public Player player = new Player(this, keyH);
 
     public GamePanel() {
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
@@ -40,7 +50,7 @@ public class GamePanel extends JPanel implements Runnable {
 
     @Override
     public void run() {
-        double drawInterval = 1000000000 / FPS;
+        double drawInterval = (double) 1000000000 / FPS;
         double nextDrawTime = System.nanoTime() + drawInterval;
 
         while (gameThread != null) {
@@ -74,7 +84,8 @@ public class GamePanel extends JPanel implements Runnable {
 
         Graphics2D g2 = (Graphics2D) g;
 
-        player.paintComponent(g2);
+        tileM.draw(g2);
+        player.draw(g2);
         g2.dispose();
     }
 }
